cffca89f898d91b0444aafe4f11d3b2967b539a1
ShyamSunder149 <SubstantialCitron0@protonmail.com>
2024-08-05 18:12:04 +0530

    Few code additions

--- main.go
+++ main.go
@@ -3,21 +3,22 @@ package main
 import (
   "fmt"
   "os"
+  "strings"
 )
 
 var globalStore = make(map[string]string)
  
 type Transaction struct {
   
-  var store map[string]string
-  var nextTransaction *Transaction
+  Store map[string]string
+  nextTransaction *Transaction
 
 }
 
 type TransactionStack struct {
 
-  var currentTransaction *Transaction
-  var size int 
+  currentTransaction *Transaction
+  size int 
 
 }
 
@@ -30,13 +31,17 @@ type KVStore interface {
 
 }
 
-func Begin() *Transaction {
-  return &Transaction(store : make(map[string]string), nextTransaction : nil,)  
+func Begin() *Transaction { 
+  return &Transaction {
+    Store : make(map[string]string), 
+    nextTransaction : nil,
+  }
 }
 
 func (ts *TransactionStack) addTransactionToStack(t *Transaction) {
-  if ts.currentTransaction == nil { ts.currentTransaction = t } 
-  else {
+  if ts.currentTransaction == nil { 
+    ts.currentTransaction = t 
+  } else {
     t.nextTransaction = ts.currentTransaction
     ts.currentTransaction = t 
   }
@@ -48,37 +53,88 @@ func (ts *TransactionStack) removeTopOfStack() {
   ts.size = ts.size - 1
 }
 
-func (t *Transaction) Set(k, v string) {
-  t.store[k] = v 
-}
+func (t *Transaction) Set(k, v string) { t.Store[k] = v }
 
 func (t *Transaction) Get(k string) {
-  v, ok := t.store[k]
-  if ok { fmt.Println("Value Unavailable") }
-  else { fmt.Printf("%s : %s", k, v) }
+  if v, ok := t.Store[k]; ok {
+    fmt.Printf("%d : %d", k, v)
+  } else if v, ok := globalStore[k]; ok {
+    fmt.Printf("%d : %d", k, v)
+  } else {
+    fmt.Println("Value Unavailable")
+  }
+}
+
+func (t * Transaction) TransferElementsToPrevStore() {
+  if t.nextTransaction == nil {
+    for k, v := range t.Store { globalStore[k] = v }
+  } else {
+    temp := t.nextTransaction
+    for k, v := range t.Store { temp.Store[k] = v } 
+  }
+}
+
+// printing inside a Transaction will only get the Transaction store keys.
+func (t *Transaction) PrintKeyValues() {
+  for k, v := range t.Store { fmt.Printf("%s : %s\n", k, v) }
+}
+
+func (t *Transaction) PrintKeys() {
+  for k, _ := range t.Store { fmt.Printf("%s\n", k) }
+}
+
+func PrintKeyValuesGlobal() {
+  for k, v := range globalStore { fmt.Printf("%s : %s\n", k, v) }
+}
+
+func PrintKeysGlobal() {
+  for k, _ := range globalStore { fmt.Printf("%s\n", k) }
 }
 
 func main() {
 
   fmt.Println("Konnichiwa, You are at K-V store v1")
+  var txn Transaction
+  var ts TransactionStack
 
   for true {
     var input string
     fmt.Printf("> ")
     fmt.Scanf("%s", &input)
+    inputSplit := strings.Split(input, " ")
     
-    switch input {
-      case "BEGIN" :  
-      case "ROLLBACK" : 
+    var k, v string 
+
+    token := inputSplit[0]
+    if len(inputSplit) > 2 {
+      k = inputSplit[1]
+      v = inputSplit[2]
+    } else if len(inputSplit) > 1 {
+      k = inputSplit[1]
+    } 
+    
+    switch token {
+      case "BEGIN" : txn := Begin() 
+      case "ROLLBACK" :  
       case "COMMIT" : 
-      case "SET" : 
-      case "GET" : 
+      case "SET" : txn.Set(k, v)
+      case "GET" : txn.Get(k)
       case "DELETE" : 
-      case "END" :  
+      case "END" :  txn.TransferElementsToPrevStore()
+                    ts.removeTopOfStack() 
       case "COUNT" : fmt.Println(len(globalStore))
-      case "PRINT KEYS" : 
-      case "PRINT VALUES" : 
-      case "PRINT KEY VALUE PAIRS" : 
+      case "PRINT KEYS" : if ts.size == 0 {
+                            PrintKeysGlobal()
+                          } else {
+                            fmt.Println("Note : Within the transaction the keys of the transaction are alone printed")
+                            txn.PrintKeys()  
+                          }
+      case "PRINT KEY VALUE PAIRS" : if ts.size == 0 {
+                                        PrintKeyValuesGlobal()
+                                     } else {
+                                        fmt.Println("Note : Within the transaction the key values of the transaction are alone printed")
+                                        txn.PrintKeyValues()
+                                     }
       case "QUIT" : fmt.Println("Closing Session, Arigato")
                     os.Exit(0)
       default : fmt.Println("Enter a valid command")
